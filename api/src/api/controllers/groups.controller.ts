831553c940cef55dc08f5f269805ab80321f5ef9
Kristjan <kristjan@tlu.ee>
2022-10-06 11:08:18 -0400

    added user group logic

---
+++ api/src/api/controllers/groups.controller.ts
@@ -0,0 +1,15 @@
+import {Request,RequestHandler,Response} from 'express';
+import * as GroupService from './../services/groups.service';
+
+export const getAllGroups: RequestHandler = async(req:Request, res:Response) => {
+    try {
+        const rooms = await GroupService.getAllGroups();
+    
+        res.status(200).json({rooms});
+      } catch (error) {
+        console.error('[groups.controller][getAllGroups][Error] ', typeof error === 'object' ? JSON.stringify(error) : error);
+        res.status(500).json({
+          message: 'There was an error when fetching information about groups'
+        });
+      }
+};
---
+++ api/src/api/queries/groups.queries.ts
@@ -0,0 +1,3 @@
+export const GroupQueries = {
+    GetAllGroups: 'SELECT * FROM nokey.user_groups'
+}
